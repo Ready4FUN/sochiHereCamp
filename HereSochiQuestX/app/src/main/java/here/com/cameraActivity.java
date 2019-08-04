d3992f1da928c5e8053aa9923ddd88c73bb6845f
Fridriiik <juravl46@gmail.com>
2019-08-04 18:26:36 +0300

    add camera

--- HereSochiQuestX/app/src/main/java/here/com/cameraActivity.java
+++ HereSochiQuestX/app/src/main/java/here/com/cameraActivity.java
@@ -10,5 +10,10 @@ public class cameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
     }
 }
