b92a4436a10f5561368b7c442aa7a7a5de8725f9
Fridriiik <juravl46@gmail.com>
2019-08-18 15:44:33 +0300

    add PICKLE RIIIIIICK

--- HereSochiQuestX/app/src/main/java/here/com/arActivity.java
+++ HereSochiQuestX/app/src/main/java/here/com/arActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.ar.sceneform.ux.ArFragment;
+
 public class arActivity extends AppCompatActivity {
+
+  private ArFragment fragment;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_ar);
+
+    fragment = (ArFragment)
+            getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
   }
 }
