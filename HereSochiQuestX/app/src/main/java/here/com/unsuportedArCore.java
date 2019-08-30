2a99dad9e41fd07197d11bdfa45de4053c239e0e
Fridriiik <juravl46@gmail.com>
2019-08-30 23:51:34 +0300

    fix AR and color

--- HereSochiQuestX/app/src/main/java/here/com/unsuportedArCore.java
+++ HereSochiQuestX/app/src/main/java/here/com/unsuportedArCore.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.ar.core.Camera;
 import com.google.ar.core.exceptions.CameraNotAvailableException;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.Node;
@@ -21,19 +23,12 @@ import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.ux.ArFragment;
 
+import java.util.HashMap;
+
 public class unsuportedArCore extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
-
-
-    private ModelRenderable andyRenderable;
-
-    private int DefRenderable;
 
     TaskManager taskManager;
 
-    SceneView mSceneView;
-    Scene mScene;
 
 
     private String lastTaskIndex;
@@ -42,7 +37,9 @@ public class unsuportedArCore extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unsuported_ar_core);
-        mSceneView = findViewById(R.id.sceneView);
+
+
+
 
 
         //по хорошему определить отдельную функцию и вызвать из разных мест...
@@ -55,17 +52,23 @@ public class unsuportedArCore extends AppCompatActivity {
 
         Button arButton = (Button)findViewById(R.id.morphArButton);
 
+        ImageView arImage= (ImageView)findViewById(R.id.questImage);
+        ImageView notAR= (ImageView)findViewById(R.id.notAr);
+
+
         taskManager = new TaskManager(this, null);
 
-        //оно не работатет -_-
-        //HashMap<String, String> questData = taskManager.getCurrentTaskDesctiption();
-        //questData.get("language")
+        HashMap<String, String> questData = taskManager.getCurrentTaskDesctiption();
+
+        String questLanguage = questData.get("language");
+        String questImg = questData.get("img_code");
 
         if(taskDone){
 
 
             //TODO вставить получение задания
-            arText.setText("ЗАДАНИЕ МНЕ ЗАПИЛИ");
+            arText.setText(questLanguage);
+            arImage.setImageResource(getResources().getIdentifier("drawable/" + questImg, null, getPackageName()));
             arButton.setText(getResources().getString(R.string.backToMap));
         } else {
             arText.setText(getResources().getString(R.string.done));
@@ -78,100 +81,30 @@ public class unsuportedArCore extends AppCompatActivity {
 
         switch (lastTaskIndex){
             case ("1"):
-                DefRenderable =  R.raw.pickle_rick;
+                notAR.setImageResource(R.drawable.annotation);
                 break;
             case ("2"):
-                DefRenderable =  R.raw.model;
+                notAR.setImageResource(R.drawable.annotation);
                 break;
             case ("3"):
-                DefRenderable =  R.raw.butthole;
+                notAR.setImageResource(R.drawable.annotation);
                 break;
             case ("4"):
-                DefRenderable =  R.raw.butterrobot;
+                notAR.setImageResource(R.drawable.annotation);
                 break;
             case ("5"):
-                DefRenderable =  R.raw.pickle_rick;
+                notAR.setImageResource(R.drawable.annotation);
                 break;
             default:
-                DefRenderable = R.raw.pickle_rick;
-                break;
-        }
-
-        mScene = new Scene(mSceneView);
-        ModelRenderable.builder()
-                .setSource(this, DefRenderable)
-                .build()
-                .thenAccept(renderable->onRenderableLoaded(renderable))
-                .exceptionally(throwable -> {
-                    Log.i("Sceneform", "failed to load model");
-                    return null;
-                });
-    }
-
-
-    private void onRenderableLoaded(Renderable renderable) {
-        Node parentNode = new Node();
-        parentNode.setRenderable(renderable);
-        parentNode.setParent(mSceneView.getScene());
-        parentNode.setLocalPosition(new Vector3(0f, -0.5f, -1f));
-        parentNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0f), 180));
-
-        //Я вот смотрю на этот код
-        // И понимаю, что его писал как минимум сверхразум
-        //ДЕЙСТВИТЕЛЬ НО БЛЯТЬ, НАХУЯ НАМ ПЕРЕОПРЕДЕЛЯТЬ ПЕРЕМЕННУЮ И ОДИН РАЗ ВЫЗВАТЬ ФУНКЦИЮ
-        //Мы лучше дохуя раз вызовем функции.
-        //Переписывать это я конечно же не буду
-        switch (lastTaskIndex){
-            case ("1"):
-                parentNode.setLocalPosition(new Vector3(0f, -0.5f, -1f));
-                parentNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0f), 180));
-                break;
-            case ("2"):
-                parentNode.setLocalPosition(new Vector3(0f, -0.7f, -1.5f));
-                parentNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0f), 35));
-                break;
-            case ("3"):
-                parentNode.setLocalPosition(new Vector3(0f, -0.5f, -1f));
-                parentNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0f), 140));
-                break;
-            case ("4"):
-                parentNode.setLocalPosition(new Vector3(0f, -0.4f, -1f));
-                parentNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0f), -90));
-                break;
-            case ("5"):
-                parentNode.setLocalPosition(new Vector3(0f, -0.5f, -1f));
-                parentNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0f), 180));
-                break;
-            default:
-                parentNode.setLocalPosition(new Vector3(0f, -0.5f, -1f));
-                parentNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0f), 180));
+                notAR.setImageResource(R.drawable.annotation);
                 break;
         }
 
 
-        mSceneView.getScene().addChild(parentNode);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        try {
-            mSceneView.resume();
-        }
-        catch (CameraNotAvailableException e){}
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mSceneView.pause();
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mSceneView.destroy();
-    }
 
 
     //как ты поняд, это просто ctrl+c ctrl+v
